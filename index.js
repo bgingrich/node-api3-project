842c45dbc7ecd3e2f310438c9b964c6756c09b93
Billy Gingrich <whgingrich@gmail.com>
2020-01-15 12:58:45 -0500

    mvp complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code away!
+const express = require('express')
+const server = express()
+const logger = require('./middleware/logger')
+const welcomeRouter = require('./routers/welcome')
+const userRouter = require('./users/userRouter')
+
+server.use(logger())
+
+server.use(express.json())
+const port = 9000
+
+server.use('/', welcomeRouter)
+server.use('/users', userRouter)
+
+server.listen(port, ()=>{
+    console.log(`\n*** Server listening on ${port}`)
+}) 
